b77470fe7cb1b1c4d61cb27bd01cf627de5c839a
Piotr Nowakowski <pitman1991@gmail.com>
2012-11-14 15:14:24 +0100

    Dodanie prostego interfejsu

--- src/pl/project/android_opengl/MainActivity.java
+++ src/pl/project/android_opengl/MainActivity.java
@@ -2,9 +2,6 @@ package pl.project.android_opengl;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
@@ -13,12 +10,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
     
 }
